835b40095793413a575a3abffd4984807d70e86f
orszaczkyanna <orszaczky.anna@gmail.com>
2025-05-20 15:00:00 +0200

    feat: add onboarding screen
    - Implemented the initial layout of the onboarding screen (index.tsx)
    - Created a reusable Logo component
    - Created a reusable CTAButton component

--- bingelog/components/ScreenWrapper.tsx
+++ bingelog/components/ScreenWrapper.tsx
@@ -7,7 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const ScreenWrapper = ({ children }: { children: ReactNode }) => {
   return (
     // Outer View sets the base background and ensures full height
-    <View className="flex-1 bg-background">
+    <View className="flex-1 bg-background py-8 px-4">
       {/* SafeAreaView keeps content away from notches/status bars */}
       <SafeAreaView className="flex-1 justify-center items-center">
         {children}

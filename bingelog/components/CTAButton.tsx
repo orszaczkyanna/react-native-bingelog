7d082ce3adb3f0c6eff716a3d07ad9b91eca1688
orszaczkyanna <orszaczky.anna@gmail.com>
2025-06-09 15:00:00 +0200

    feat: add initial frontend-backend integration for user registration
    - Used built-in JavaScript fetch API to send POST request
    - Displayed registration responses using React Native alert dialog
    - Added isLoading prop to CTAButton to disable it during form submission

--- bingelog/components/CTAButton.tsx
+++ bingelog/components/CTAButton.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Pressable, Text } from "react-native";
 
-interface CTAButtonProps {
+interface Props {
   title: string;
   onPress: () => void;
+  isLoading?: boolean;
   pressableClassName?: string;
 }
 
-const CTAButton = ({ title, onPress, pressableClassName }: CTAButtonProps) => {
+const CTAButton = ({
+  title,
+  onPress,
+  isLoading,
+  pressableClassName,
+}: Props) => {
   return (
     <Pressable
+      className={`bg-accent min-h-16 w-[90vw] rounded-md justify-center items-center active:opacity-70 ${
+        isLoading ? "opacity-50" : ""
+      } ${pressableClassName}`}
       onPress={onPress}
-      className={`bg-accent min-h-16 w-[90vw] rounded-md justify-center items-center active:opacity-70 ${pressableClassName}`}
+      disabled={isLoading}
     >
       <Text className="font-nunitoBold text-background text-2xl">{title}</Text>
     </Pressable>
